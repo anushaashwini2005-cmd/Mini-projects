1830216e570140b3c5162f7f8f42f6a530054a9d
Anusha N <anushaashwini2005@gmail.com>
2026-01-12 21:17:16 +0530

    Implement simple calculator with basic operations

---
+++ Simple_calculator.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the 1st number: "))
+b=int(input("Enter the 2nd number: "))
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+floor=a//b
+exp=a**b
+print("Addition of two number: ",sum)
+print("Difference of two number: ",sub)
+print("Multiplication of two number: ",mul)
+print("Division of two number: ",div)
+print("Floor value: ",floor)
+print("Exponential value: ",exp)
